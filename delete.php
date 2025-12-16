017cf699950be6df34dc4d5627c7f775324442b6
Serpiel <hmarujo1510@gmail.com>
2025-12-16 01:57:15 +0100

    Fixin intern bugs

--- delete.php
+++ delete.php
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<?php 
+<?php
 session_start();
 
 if (!isset($_SESSION['admin'])) {
@@ -29,36 +28,5 @@ try {
 } catch (PDOException $e) {
     echo "Error : " . $e->getMessage();
 }
-=======
-<?php 
-session_start();
 
-if (!isset($_SESSION['admin'])) {
-    header("Location: admin.php");
-    exit();
-}
-
-include 'bdd.php';
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$dbusername, $dbpassword);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    if (isset($_GET['table']) && isset($_GET['id'])) {
-        $table = $_GET['table'];
-        $id = $_GET['id'];
-
-        $stmt = $conn->prepare("DELETE FROM $table WHERE id = :id");
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        $stmt->execute();
-
-        header("Location: admin_dashboard.php?table=$table&success=1");
-        exit();
-    } else {
-        echo "Table or ID missing.";
-    }
-} catch (PDOException $e) {
-    echo "Error : " . $e->getMessage();
-}
->>>>>>> 2fca341a50f4c42f33410b1d0aa0c44a74c8e042
 ?>
